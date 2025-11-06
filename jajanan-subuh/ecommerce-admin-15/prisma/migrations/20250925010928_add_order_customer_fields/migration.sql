2ff9173c332b16e5e6fa051c0357bfe90c97a295
jajanan subuh <jajanansubuh6@gmail.com>
2025-11-06 15:07:52 +0700

    chore: remove nested duplicate folder jajanan-subuh/ecommerce-admin-15 to avoid double-build

--- jajanan-subuh/ecommerce-admin-15/prisma/migrations/20250925010928_add_order_customer_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Order" ADD COLUMN     "address" TEXT,
-ADD COLUMN     "customerName" TEXT,
-ADD COLUMN     "paymentMethod" TEXT;
--- jajanan-subuh/ecommerce-admin-15/prisma/migrations/20251030045637_add_store_methods_and_review_name/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Review" ADD COLUMN     "name" TEXT;
-
--- AlterTable
-ALTER TABLE "Store" ADD COLUMN     "paymentMethods" JSONB,
-ADD COLUMN     "shippingMethods" JSONB;
